6ece7bb791511e4d9bbe98bfb89d0eba4757b0a7
LeandroCristianoM <leandro.cristiano523@comunidadunir.net>
2024-02-07 21:37:25 -0500

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+:
+
+Dockerfile,6/6/6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+f
+6src/main/java/com/unir/gateway/GatewayApplication.java,6/b/6b8c7878aa7ffd3a3de70c4943507334e5a68d69
---
+++ src/main/java/com/unir/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.unir.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		// Retrieve execution profile from environment variable. Otherwise, default profile is used.
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
